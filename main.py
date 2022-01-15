41d0f124ca7707bb2276e12061157d2515fa7a41
Bag Boy <73358379+BAGBOY555@users.noreply.github.com>
2022-01-16 01:06:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,13 @@ from bot import CONFIG, COMMAND, LOCAL, LOGGER, STATUS
 from bot.handlers import *
 import asyncio
 
+# Initialize bot
+app = Client(
+    "Bot",
+    bot_token=CONFIG.BOT_TOKEN,
+    api_id=CONFIG.API_ID,
+    api_hash=CONFIG.API_HASH,
+    workdir=os_path_join(CONFIG.ROOT, CONFIG.WORKDIR),
+    plugins=dict(root="bot/handlers")
+)
+app.set_parse_mode("html")
